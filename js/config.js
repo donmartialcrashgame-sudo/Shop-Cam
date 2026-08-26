12f967cb532edf414a2d39051af0979de37ca3be
game_api <donmartialcrashgame@gmail.com>
2026-08-26 23:19:36 +0100

    feat: add web push public key configuration

--- js/config.js
+++ js/config.js
@@ -2,3 +2,6 @@ export const SUPABASE_URL = 'https://xgxmnhqdxhvguoplbpjv.supabase.co';
 export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_E-5Eg78QR6jTSULAWkhG5w_4B0XGGnp';
 export const AUTH_CALLBACK_URL = `${window.location.origin}/callback.html`;
 export const DASHBOARD_URL = `${window.location.origin}/dashboard.html`;
+
+// Public VAPID key used by Web Push on Android, iOS PWA and desktop browsers.
+export const VAPID_PUBLIC_KEY = 'BHYI4ekL9MSloyJ_m1-ncZoFq3T2Jn57ryqXGaImLe_HfCSytNbTztu3rqx15NSH5OQ8-kk86MYi1FFg9fJPxpA';
