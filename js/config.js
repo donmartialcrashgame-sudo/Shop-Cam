4f932a7b8c95206ae4ceefd128760bd39f547b8b
game_api <donmartialcrashgame@gmail.com>
2026-08-26 16:19:11 +0100

    feat: add shared Supabase configuration

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = 'https://xgxmnhqdxhvguoplbpjv.supabase.co';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_E-5Eg78QR6jTSULAWkhG5w_4B0XGGnp';
+export const AUTH_CALLBACK_URL = `${window.location.origin}/callback.html`;
+export const DASHBOARD_URL = `${window.location.origin}/dashboard.html`;
